410fe717f3266984f42f3bfd928b315695d5595c
Camille <ingouf.camille@gmail.com>
2015-11-20 00:37:36 +0100

    Ajout image écran

--- Dooble/SymboleG.java
+++ Dooble/SymboleG.java
@@ -1,5 +1,10 @@
+import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.Point;
+import java.io.IOException;
+import java.net.URL;
+
+import javax.imageio.ImageIO;
 
 /**
  * Cette classe représente un symbole à afficher.
@@ -12,7 +17,7 @@ import java.awt.Point;
 public class SymboleG extends Element{
 
 	/* Attributs */
-	
+
 	private Symbole symbole;
 
 	private Image img;
@@ -20,14 +25,32 @@ public class SymboleG extends Element{
 	private int largeur;
 	private int hauteur;
 	
+	private Point min;
+	private Point max;
+	
 	private boolean selectionne;
 
 	/* Constructeur */
 	
-	public SymboleG(Point o) {
+	public SymboleG(Point o, Symbole s, int largeur, int hauteur) {
 		super(o);
 		selectionne = false;
+		symbole = s;
+		this.largeur = largeur;
+		this.hauteur = hauteur;
+		min = new Point((int)o.getX()-largeur/2,(int)o.getY()-hauteur/2);
+		max = new Point((int)o.getX()+largeur/2,(int)o.getY()+hauteur/2);
+		
 		//TODO Générer l'image
+		
+		try {
+		    URL url = getClass().getClassLoader().getResource ("./img/"+s.getLienImg());
+		    img = ImageIO.read (url);
+		}
+		catch ( IOException e ) {		
+		    System.out.println ("[!] Erreur : L'image est introuvable.\n" + e);
+		}
+		
 	}
 	
 	/* Accesseur */
@@ -42,5 +65,11 @@ public class SymboleG extends Element{
 	
 	public void setSelectionne(boolean b){
 		selectionne = b;
+	}
+	
+	/* Méthode */
+	
+	public void afficher(Graphics g){
+		g.drawImage (img, (int)min.getX(), (int)(g.getClipBounds().getHeight()-this.min.getY()-hauteur), largeur, hauteur, null);
 	}
 }
